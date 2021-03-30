d64036c790b9ec5d2c8afcd755152be356b98072
Alvaro217 <78560621+Alvaro217@users.noreply.github.com>
2021-03-31 02:18:49 +0300

    revision

--- main.py
+++ main.py
@@ -7,12 +7,22 @@ class Window:
     WIDTH = 1000
     HEIGHT = 800
 
+    COLOR = {'white': (255, 255, 255),
+             'black': (0, 0, 0),
+             'yellow': (255, 255, 0)}
+
+    NEW_LEVEL = ['Score:', 'Circles:', 'Squares:']
+
+    RULES = {'Circle: 30': 'Press: c',
+             'Square: 20': 'Press: s',
+             'Score booster: 100': 'Press: b'}
+
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         pygame.display.set_caption("Clicker")
-        self.screen.fill((255, 255, 0))
-        self.write_template()
+        self.screen.fill(self.COLOR['black'])
+        self.template()
         pygame.display.flip()
 
     def draw_obj(self, shape, color, x, y):
@@ -21,43 +31,48 @@ class Window:
         if shape == 'square':
             pygame.draw.rect(self.screen, color, [x, y, 50, 50])
 
-    def write_template(self, score=0, level=1):
+    def template(self, score=0, level=1):
         font = pygame.font.Font('freesansbold.ttf', 40)
-        score_text = font.render('Your score is: {}'.format(score), True, (255, 255, 255), (0, 0, 0))
-        level_text = font.render('Level: {}'.format(level), True, (255, 255, 255), (0, 0, 0))
+
+        score_text = font.render('Your score is: {}'.format(score), True,
+                                 self.COLOR['white'], self.COLOR['black'])
+        level_text = font.render('Level: {}'.format(level), True,
+                                 self.COLOR['white'], self.COLOR['black'])
+
         self.screen.blit(score_text, (5, 15))
         self.screen.blit(level_text, (500, 15))
 
-    def write_win(self):
+    def win(self):
         font = pygame.font.Font('freesansbold.ttf', 50)
-        text = font.render('You are winner! Well done!', True, (255, 255, 255), (0, 0, 0))
+        text = font.render('You are winner! Well done!',
+                           True, self.COLOR['white'], self.COLOR['black'])
         self.screen.blit(text, (200, 400))
 
-    def write_next_level(self, score, circles, squares):
+    def next_level(self, args):
         font = pygame.font.Font('freesansbold.ttf', 20)
-        text = font.render('To next level you need:', True, (255, 255, 255), (0, 0, 0))
-        score_text = font.render('Score: {}'.format(score), True, (255, 255, 255), (0, 0, 0))
-        circle_text = font.render('Circles: {}'.format(circles), True, (255, 255, 255), (0, 0, 0))
-        square_text = font.render('Squares: {}'.format(squares), True, (255, 255, 255), (0, 0, 0))
-        self.screen.blit(text, (750, 5))
-        self.screen.blit(score_text, (750, 25))
-        self.screen.blit(circle_text, (750, 50))
-        self.screen.blit(square_text, (750, 75))
 
-    def write_rules(self):
+        text = font.render('To next level you need:',
+                           True, self.COLOR['white'], self.COLOR['black'])
+
+        self.screen.blit(text, (750, 5))
+
+        for i in range(len(self.NEW_LEVEL)):
+            text = font.render('{} {}'.format(self.NEW_LEVEL[i], args[i]),
+                               True, self.COLOR['white'], self.COLOR['black'])
+            self.screen.blit(text, (750, 25 * (i + 1)))
+
+    def rules(self):
         font = pygame.font.Font('freesansbold.ttf', 30)
-        score_text = font.render('Score booster: 100', True, (255, 255, 255), (0, 0, 0))
-        circle_text = font.render('Circle: 30', True, (255, 255, 255), (0, 0, 0))
-        square_text = font.render('Square: 20', True, (255, 255, 255), (0, 0, 0))
-        score_press = font.render('Press: b', True, (255, 255, 255), (0, 0, 0))
-        circle_press = font.render('Press: c', True, (255, 255, 255), (0, 0, 0))
-        square_press = font.render('Press: s', True, (255, 255, 255), (0, 0, 0))
-        self.screen.blit(score_text, (50, 710))
-        self.screen.blit(circle_text, (400, 710))
-        self.screen.blit(square_text, (700, 710))
-        self.screen.blit(score_press, (50, 745))
-        self.screen.blit(circle_press, (400, 745))
-        self.screen.blit(square_press, (700, 745))
+        i = 0
+        for item in self.RULES:
+            text = font.render(item, True,
+                               self.COLOR['white'], self.COLOR['black'])
+            text_press = font.render(self.RULES[item], True,
+                                     self.COLOR['white'], self.COLOR['black'])
+
+            self.screen.blit(text, (50 + 300 * i, 710))
+            self.screen.blit(text_press, (50 + 300 * i, 745))
+            i += 1
 
 
 class Object:
@@ -65,7 +80,8 @@ class Object:
     Vy = 2
     x = 500
     y = 400
-    colors = [(0, 225, 0), (0, 0, 255), (255, 0, 0), (255, 0, 255), (0, 255, 255), (255, 255, 0)]
+    colors = [(0, 225, 0), (0, 0, 255), (255, 0, 0),
+              (255, 0, 255), (0, 255, 255), (255, 255, 0)]
 
     def __init__(self, shape):
         self.color = random.choice(self.colors)
@@ -73,19 +89,19 @@ class Object:
         self.Vy = random.randint(1, 3)
         self.shape = shape
 
+    def change_v(self, x1, y1, x2, y2):
+        if self.x >= x1 or self.x <= x2:
+            self.Vx = -self.Vx
+        if self.y >= y1 or self.y <= y2:
+            self.Vy = -self.Vy
+
     def move(self):
         self.x += self.Vx
         self.y += self.Vy
         if self.shape == 'circle':
-            if self.x >= 975 or self.x <= 25:
-                self.Vx = -self.Vx
-            if self.y >= 680 or self.y <= 120:
-                self.Vy = -self.Vy
+            self.change_v(975, 680, 25, 120)
         else:
-            if self.x >= 950 or self.x <= 0:
-                self.Vx = -self.Vx
-            if self.y >= 660 or self.y <= 110:
-                self.Vy = -self.Vy
+            self.change_v(950, 660, 0, 110)
 
     def draw(self):
         self.move()
@@ -97,14 +113,12 @@ class Game(Window):
     score = 0
     level = 1
     boost = 2
-    circles = []
-    squares = []
+    obj = [[], []]
+    cost = {pygame.K_c: [30, ['circle', 0]],
+            pygame.K_s: [20, ['square', 1]],
+            pygame.K_b: [100, 2]}
     # score, circles, squares
-    levels = [[100, 1, 1],
-              [1000, 5, 10],
-              [10000, 30, 25],
-              [100000, 45, 60],
-              [1000000, 100, 100]]
+    levels = [[100 * 10 ** i, 4 * i + 5, 7 * i + 4] for i in range(5)]
 
     def __init__(self):
         super().__init__()
@@ -119,53 +133,51 @@ class Game(Window):
                     if not self.win:
                         if event.key == pygame.K_SPACE:
                             self.score += self.boost
-                        if event.key == pygame.K_c:
-                            if self.score >= 30:
-                                self.circles.append(Object('circle'))
-                                self.score -= 30
-                        if event.key == pygame.K_s:
-                            if self.score >= 20:
-                                self.squares.append((Object('square')))
-                                self.score -= 20
-                        if event.key == pygame.K_b:
-                            if self.score >= 100:
-                                self.boost += 2
-                                self.score -= 100
+                        else:
+                            try:
+                                t = event.key
+                                if self.score >= self.cost[t][0]:
+                                    self.score -= self.cost[t][0]
+                                    L = self.cost[t][1]
+                                    if isinstance(L, type([])):
+                                        self.obj[L[1]].append(Object(L[0]))
+                                    else:
+                                        self.boost += 2
+                            except Exception:
+                                pass
                     else:
                         if event.key == pygame.K_SPACE:
                             self.boost = 2
                             self.score = 0
                             self.level = 1
                             self.win = False
-                            self.circles.clear()
-                            self.squares.clear()
+                            self.obj[0].clear()
+                            self.obj[1].clear()
 
             self.screen.fill((0, 0, 0))
-            super().write_template(self.score, self.level)
-            super().write_rules()
+            super().template(self.score, self.level)
+            super().rules()
 
-            for figure in self.circles:
-                l = figure.draw()
-                super().draw_obj(l[0], l[1], l[2], l[3])
-
-            for figure in self.squares:
-                l = figure.draw()
-                super().draw_obj(l[0], l[1], l[2], l[3])
+            for array in self.obj:
+                for figure in array:
+                    l = figure.draw()
+                    super().draw_obj(l[0], l[1], l[2], l[3])
 
             if self.level >= 5:
                 self.win = True
-                super().write_win()
+                super().win()
             else:
                 if self.score >= self.levels[self.level - 1][0]:
-                    if len(self.circles) >= self.levels[self.level - 1][1]:
-                        if len(self.squares) >= self.levels[self.level - 1][2]:
+                    if len(self.obj[0]) >= self.levels[self.level - 1][1]:
+                        if len(self.obj[1]) >= self.levels[self.level - 1][2]:
                             self.level += 1
-                            self.squares.clear()
-                            self.circles.clear()
+                            self.obj[0].clear()
+                            self.obj[1].clear()
                             self.score = 0
-                super().write_next_level(max(self.levels[self.level - 1][0] - self.score, 0),
-                                         max(self.levels[self.level - 1][1] - len(self.circles), 0),
-                                         max(self.levels[self.level - 1][2] - len(self.squares), 0))
+                level = self.levels[self.level - 1]
+                super().next_level([max(level[0] - self.score, 0),
+                                    max(level[1] - len(self.obj[0]), 0),
+                                    max(level[2] - len(self.obj[1]), 0)])
 
             pygame.display.flip()
 
